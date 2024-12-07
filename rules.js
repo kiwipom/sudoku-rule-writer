8a2b1590f4282ad58e8968620c1dfb0bdf041b2a
randall <randall.does.puzzles@gmail.com>
2024-12-07 22:54:07 +1300

    Changing up a few things
    - Put the buttons by section on the left
    - Put the outputs on the right
    - Split the rules into sections by scope

--- rules.js
+++ rules.js
@@ -1,89 +1,144 @@
-ruleset={
-  scopes: ["main","global","outside","lines","other","neighbours"],
-  rules:[
-    {
-      "id"    : "sudoku",
-      "title" : "Sudoku",
-      "scope" : "main",
-			"symbol": "",
-      "order" : "1",
-      "descriptions" : [{
-        "title": "standard",
-        "text" : "Normal 6x6 Sudoku rules apply. Place the digits 1-6 exactly once in every row, column, and 2x3 box.",
-        "html" : "<li><b>Normal 6x6 Sudoku</b> rules apply. Place the digits 1-6 exactly once in every row, column, and 2x3 box.</li>"
-      },{
-        "title": "irregular",
-        "text" : "Irregular 6x6 Sudoku rules apply. Place the digits 1-6 exactly once in every row, column, and bold-outlined region.",
-        "html" : "<li><b>Irregular 6x6 Sudoku</b> rules apply. Place the digits 1-6 exactly once in every row, column, and bold-outlined region.</li>"
-      },{
-        "title": "chaos",
-        "text" : "6x6 Chaos Construction rules apply: Divide the grid into 6 orthogonally connected regions with 6 cells each. Place the digits 1-6 exactly once into every row, column, and region. It is up to the solver to determine where these regions are.",
-        "html" : "<li><b>6x6 Chaos Construction</b> rules apply: Divide the grid into 6 orthogonally connected regions with 6 cells each. Place the digits 1-6 exactly once into every row, column, and region. It is up to the solver to determine where these regions are.</li>"
+let scopes = ["main", "global", "outside", "lines", "other", "neighbours"];
+
+let ruleset = [
+  {
+    "scope": "main",
+    "rules": [
+      {
+        "id": "sudoku",
+        "title": "Sudoku",
+        "symbol": "",
+        "order": "1",
+        "descriptions": [{
+          "title": "Normal 6x6 Sudoku",
+          "text": "Place the digits 1-6 exactly once in every row, column, and 2x3 box."
+        }, {
+          "title": "Irregular 6x6 Sudoku",
+          "text": "Place the digits 1-6 exactly once in every row, column, and bold-outlined region."
+        }, {
+          "title": "Chaos Construction",
+          "text": "Divide the grid into 6 orthogonally connected regions with 6 cells each. Place the digits 1-6 exactly once into every row, column, and region. It is up to the solver to determine where these regions are."
+        }]
       }]
-    },{
-      "id"    : "diagonal",
-      "title" : "Diagonal",
-      "scope" : "global",
-			"symbol": "",
-      "order" : "1",
-      "descriptions" : [{
-        "title" : "sudoku-x",
-        "text" : "Sudoku-X: Place the digits 1-6 exactly once on the marked diagonals.",
-        "html" : "<li><b>Sudoku-X</b>: Place the digits 1-6 exactly once on the marked diagonals.</li>"
-      },{
-        "title": "diagonal",
-        "text" : "Diagonal: Place the digits 1-6 exactly once on the marked diagonal.",
-        "html" : "<li><b>Diagonal:</b> Place the digits 1-6 exactly once on the marked diagonal.</li>"
-      }]
-    },{
-      "id"    : "disjoint",
-      "title" : "Disjoint",
-      "scope" : "global",
-			"symbol": "",
-      "order" : "2",
-      "descriptions" : [{
-        "text" : "Disjoint: Digits cannot repeat if they occupy the same relative position within a box.",
-        "html" : "<li><b>Disjoint:</b> Digits cannot repeat if they occupy the same relative position within a box.</li>"
-      }]
-    },{
-      "id"    : "anti-knight",
-      "title" : "Anti-knight",
-      "scope" : "global",
-			"symbol": "",
-      "order" : "3",
-      "descriptions" : [{
-        "text" : "Anti-Knight: Cells separated by a knight's move (as in chess) cannot contain the same digit as each other. A knight in chess moves 1 in one direction, and 2 in another direction, making an L shape.",
-        "html" : "<li><b>Anti-Knight:</b> Cells separated by a knight's move (as in chess) cannot contain the same digit as each other. A knight in chess moves 1 in one direction, and 2 in another direction, making an L shape.</li>"
-      }]
-    },{
-      "id"    : "anti-king",
-      "title" : "Anti-king",
-      "scope" : "global",
-			"symbol": "",
-      "order" : "4",
-      "descriptions" : [{
-        "text" : "Anti-King: Cells that are touching (share a border or corner) cannot contain the same digit as each other. It is called 'King' because that is how a King moves in chess.",
-        "html" : "<li><b>Anti-King:</b> Cells that are touching (share a border or corner) cannot contain the same digit as each other. It is called 'King' because that is how a King moves in chess.</li>"
-      }]
-    },{
-      "id"    : "noncon",
-      "title" : "Nonconsecutive",
-      "scope" : "global",
-			"symbol": "",
-      "order" : "5",
-      "descriptions" : [{
-        "text" : "Nonconsecutive: Orthogonally adjacent cells may not contain consecutive digits, meaning they may not have a difference of 1.",
-        "html" : "<li><b>Nonconsecutive:</b> Orthogonally adjacent cells may not contain consecutive digits, meaning they may not have a difference of 1.</li>"
-      }]
-    },{
-      "id"    : "fog",
-      "title" : "Fog",
-      "scope" : "global",
-			"symbol": "",
-      "order" : "1",
-      "descriptions" : [{
-        "text" : "Fog: Some cells are hidden by 'fog' meaning you cannot see their contents. The fog will be revealed when an adjacent digit is correctly filled. The intent of these puzzles is to make logical moves rather than guessing and using the fog reveal to know whether you are correct.",
-        "html" : "<li><b>Fog:</b> Some cells are hidden by 'fog' meaning you cannot see their contents. The fog will be revealed when an adjacent digit is correctly filled. The intent of these puzzles is to make logical moves rather than guessing and using the fog reveal to know whether you are correct.</li>"
-    }]
-  }]
-}
+  }, {
+    "scope": "global",
+    "rules": [
+      {
+        "id": "diagonal",
+        "title": "Diagonal",
+        "symbol": "",
+        "order": "1",
+        "descriptions": [{
+          "title": "Sudoku-x",
+          "text": "Place the digits 1-6 exactly once on the marked diagonals."
+        }, {
+          "title": "Diagonal",
+          "text": "Place the digits 1-6 exactly once on the marked diagonal."
+        }]
+      }, {
+        "id": "disjoint",
+        "title": "Disjoint",
+        "symbol": "",
+        "order": "2",
+        "descriptions": [{
+          "text": "Digits cannot repeat if they occupy the same relative position within a box."
+        }]
+      }, {
+        "id": "anti-knight",
+        "title": "Anti-knight",
+        "symbol": "",
+        "order": "3",
+        "descriptions": [{
+          "text": "Cells separated by a knight's move (as in chess) cannot contain the same digit as each other. A knight in chess moves 1 in one direction, and 2 in another direction, making an L shape."
+        }]
+      }, {
+        "id": "anti-king",
+        "title": "Anti-king",
+        "symbol": "",
+        "order": "4",
+        "descriptions": [{
+          "text": "Cells that are touching (share a border or corner) cannot contain the same digit as each other. It is called 'King' because that is how a King moves in chess."
+        }]
+      }, {
+        "id": "noncon",
+        "title": "Nonconsecutive",
+        "symbol": "",
+        "scope": "glosbal",
+        "order": "5",
+        "descriptions": [{
+          "text": "Orthogonally adjacent cells may not contain consecutive digits, meaning they may not have a difference of 1."
+        }]
+      }, {
+        "id": "fog",
+        "title": "Fog",
+        "symbol": "",
+        "order": "6",
+        "descriptions": [{
+          "text": "Some cells are hidden by 'fog' meaning you cannot see their contents. The fog will be revealed when an adjacent digit is correctly filled. The intent of these puzzles is to make logical moves rather than guessing and using the fog reveal to know whether you are correct."
+        }]
+      }
+    ]
+  }, {
+    "scope": "domino",
+    "rules": [
+      {
+        "id": "kropki",
+        "title": "Kropki",
+        "symbol": "",
+        "order": "1",
+        "descriptions": [{
+          "title": "Kropki Pairs",
+          "text": "Digits separated by a white dot are consecutive. Digits separated by a black dot have a 1:2 ratio, meaning one is exactly double the other. Not all dots are necessarily given."
+        }, {
+          "title": "Kropki Pairs (-ve)",
+          "text": "Digits separated by a white dot are consecutive. Digits separated by a black dot have a 1:2 ratio, meaning one is exactly double the other. All possible dots are given."
+        }, {
+          "title": "Difference Dots",
+          "text": "Digits separated by a white dot are consecutive. Not all dots are necessarily given."
+        }, {
+          "title": "Difference Dots (-ve)",
+          "text": "Digits separated by a white dot are consecutive. All possible dots are given."
+        }, {
+          "title": "Ratio Dots",
+          "text": "Digits separated by a black dot have a 1:2 ratio, meaning one is exactly double the other. Not all dots are necessarily given."
+        }, {
+          "title": "Ratio Dots (-ve)",
+          "text": "Digits separated by a black dot have a 1:2 ratio, meaning one is exactly double the other. All possible dots are given."
+        }]
+      }, {
+        "id": "xv",
+        "title": "XV",
+        "symbol": "",
+        "order": "2",
+        "descriptions": [{
+          "title": "XV Sums",
+          "text": "Digits separated by a V must sum to 5, and digits separated by an X must sum to 10. V and X are the Roman numerals for 5 and 10, respectively."
+        }, {
+          "title": "Only X clues",
+          "label": "XV Sums",
+          "text": "Digits separated by an X must sum to 10. X is the roman numeral for 10."
+        }, {
+          "title": "Only V clues",
+          "label": "XV Sums",
+          "text": "Digits separated by a V must sum to 5. V is the roman numeral for 5."
+        }]
+      }
+    ]
+  }, {
+    "scope": "line",
+    "rules": [
+    ]
+  }, {
+    "scope": "outside",
+    "rules": [
+    ]
+  }, {
+    "scope": "other",
+    "rules": [
+    ]
+  }
+]
+
+
+
+
